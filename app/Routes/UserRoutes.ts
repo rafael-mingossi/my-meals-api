6eb08a3a1f378d8270803cc26214302d5c65ed7f
rafael-mingossi <rafaelmingossi@gmail.com>
2025-05-03 07:47:54 +1000

    fix User routes

--- app/Routes/UserRoutes.ts
+++ app/Routes/UserRoutes.ts
@@ -2,11 +2,11 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.group(() => {
-    Route.get('/:id', 'UsersController.get')
-    Route.put('/edit', 'UsersController.edit')
-    Route.delete('/:id', 'UsersController.delete')
-    Route.post('/notification-token', 'UsersController.updateNotificationToken')
     Route.delete('/notification-token', 'UsersController.deleteNotificationToken')
+    Route.post('/notification-token', 'UsersController.updateNotificationToken')
+    Route.put('/edit', 'UsersController.edit')
+    Route.get('/:id', 'UsersController.get')
+    Route.delete('/:id', 'UsersController.delete')
   })
     .prefix('/users')
     .middleware('auth:jwt')
